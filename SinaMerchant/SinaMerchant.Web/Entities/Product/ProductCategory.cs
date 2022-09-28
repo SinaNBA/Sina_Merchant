536ddef122a0f7f2ac90e753891175f8f3145a80
Sina Nejadbakhtiari <snejadbakhtiari@gmail.com>
2022-09-28 12:09:49 +0200

    Update ProductCategory.cs

--- SinaMerchant/SinaMerchant.Web/Entities/Product/ProductCategory.cs
+++ SinaMerchant/SinaMerchant.Web/Entities/Product/ProductCategory.cs
@@ -3,7 +3,7 @@
     public class ProductCategory
     {
         public int Id { get; set; }
-        public int ParentId { get; set; } // a self-join( a foreign key to another category's ID record)
+        public int? ParentId { get; set; } // a self-join( a foreign key to another category's ID record)
         public string Name { get; set; } // product category's name
     }
 }
